c2a206d3dce5eb7c7d01970643c40a0525cadc5d
Matti Ermold <mattiermold@gmail.com>
2019-03-06 20:04:59 +0100

    Global R imports data file with swedish charackters correctly

--- global.R
+++ global.R
@@ -10,6 +10,6 @@ library(shinydashboard)
 
 setwd("C:/Users/mermo/Downloads/skydsvardtrad")
 
-sktradjkp <- read_csv("C:/Users/mermo/Downloads/! strad/sktradjkp.csv")
+sktradjkp <- read_csv("C:/Users/mermo/Downloads/! strad/sktradjkp.csv",locale = locale(encoding = "ISO-8859-1"))
 
 str(sktradjkp)
