5d710133fed59f3d5da0bde9ffeb01d6bee8661d
Matti Ermold <mattiermold@gmail.com>
2019-03-06 21:11:06 +0100

    Server uptadet to render bargraph and adjust labels

--- server.R
+++ server.R
@@ -2,12 +2,21 @@
 
 shinyServer(function(input, output, session) 
   
+{  
+  
+# Add visualzations od both datatable and graphics
+  
 output$DataSk <- DT::renderDataTable(DT::datatable(sktradjkp, rownames = FALSE, 
                                                      options=list(scrollX=TRUE)
     )
-  )
 )
-  
+
+output$BarPlot <- renderPlot({
+  ggplot(sktradjkp, aes_string(x=input$StrToPlot, y=input$IntToPlot)) + 
+    stat_summary(fun.y="mean", geom="bar") + theme(axis.text.x = element_text(angle = 45, hjust = 1,size=12))
+   
+    })   
+})
   
 
   
